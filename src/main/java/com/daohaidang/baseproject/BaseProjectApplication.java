b2e2cb4c49080f647b9727fa0c11e67c5017d85a
Dao Hai Dang <daohaidangpc@gmail.com>
2025-07-28 16:02:14 +0700

    feat(webconfig): update cors config

--- src/main/java/com/daohaidang/baseproject/BaseProjectApplication.java
+++ src/main/java/com/daohaidang/baseproject/BaseProjectApplication.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 //@EnableCaching
+
 public class BaseProjectApplication {
 
     public static void main(String[] args) {
